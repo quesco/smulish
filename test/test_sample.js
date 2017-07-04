cfc4a0d665b270a49d1b34a7ece0f3d5500d3fbf
carlosfranco <carloseduardo.rorizfranco@gmail.com>
2017-07-04 14:19:30 -0300

    Including tests framework: Mocha and Chai

---
+++ test/test_sample.js
@@ -0,0 +1,29 @@
+var assert = require('assert');
+var assertChai = require('chai').assert
+  , expectChai = require('chai').expect
+  , foo = 'bar'
+  , beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+
+
+    it('should be test using chai library', function() {
+      assertChai.typeOf(foo, 'string'); // without optional message
+      assertChai.typeOf(foo, 'string', 'foo is a string'); // with optional message
+      assertChai.equal(foo, 'bar', 'foo equal `bar`');
+      assertChai.lengthOf(foo, 3, 'foo`s value has a length of 3');
+      assertChai.lengthOf(beverages.tea, 3, 'beverages has 3 types of tea');
+    });
+
+    it('should be test using chai library like a BDD test using through "expect" interface', function() {
+      expectChai(foo).to.be.a('string');
+      expectChai(foo).to.equal('bar');
+      expectChai(foo).to.have.lengthOf(3);
+      expectChai(beverages).to.have.property('tea').with.lengthOf(3);
+    });
+  });
+});
