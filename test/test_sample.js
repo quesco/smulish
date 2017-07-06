83d8353d923b7faed35b91e228efb41e34bcd1ff
Ramon Marques <ramonpm@gmail.com>
2017-07-06 20:01:55 -0300

    Fixing .jshintrc to not show error on undefined variables and functions in the file, that may be defined else where

--- test/test_sample.js
+++ test/test_sample.js
@@ -1,17 +1,17 @@
 var assert = require('assert');
-var assertChai = require('chai').assert
-  , expectChai = require('chai').expect
-  , foo = 'bar'
-  , beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
+var assertChai = require('chai').assert,
+  expectChai = require('chai').expect,
+  foo = 'bar',
+  beverages = {tea: ['chai', 'matcha', 'oolong']};
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1, 2, 3].indexOf(4));
     });
 
 
-    it('should be test using chai library', function() {
+    it('should be test using chai library', function () {
       assertChai.typeOf(foo, 'string'); // without optional message
       assertChai.typeOf(foo, 'string', 'foo is a string'); // with optional message
       assertChai.equal(foo, 'bar', 'foo equal `bar`');
@@ -19,7 +19,7 @@ describe('Array', function() {
       assertChai.lengthOf(beverages.tea, 3, 'beverages has 3 types of tea');
     });
 
-    it('should be test using chai library like a BDD test through "expect" interface', function() {
+    it('should be test using chai library like a BDD test through "expect" interface', function () {
       expectChai(foo).to.be.a('string');
       expectChai(foo).to.equal('bar');
       expectChai(foo).to.have.lengthOf(3);
